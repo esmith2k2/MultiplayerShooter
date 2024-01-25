6dd10491022c96b8cac4c7a3622c6bbb9f863aa7
Evan Smith <esmith2k2@gmail.com>
2024-01-25 18:01:34 -0500

    Setup for multiplayer shooter project.

---
+++ ShooterGame/Character/ShooterAnimInstance.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API UShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime);
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"));
+	class AShooterCharacter* ShooterCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"));
+	float Speed;
+
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"));
+	bool bIsInAir;
+
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"));
+	bool bIsAccelerating;
+
+};
