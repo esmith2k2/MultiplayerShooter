7e5a9d0832874cb898f2ac11aca517701ed55d53
Evan Smith <esmith2k2@gmail.com>
2024-10-22 00:06:55 -0400

    feat: add shell physics, sounds, and lifetime. fix: camera no longer collides with shell casing

--- ShooterGame/Weapon/BulletCasing.cpp
+++ ShooterGame/Weapon/BulletCasing.cpp
@@ -2,6 +2,8 @@
 
 
 #include "BulletCasing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 ABulletCasing::ABulletCasing()
 {
@@ -9,11 +11,32 @@ ABulletCasing::ABulletCasing()
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bullet Casing"));
 	SetRootComponent(CasingMesh);
+	CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+	ShellEjectionImpulse = 10.f;
 
 }
 
 void ABulletCasing::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CasingMesh->OnComponentHit.AddDynamic(this, &ABulletCasing::OnHit);
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+
+	SetLifeSpan(3.f);
+	
+}
+
+void ABulletCasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) 
+{
+	if(ShellSound)
+	{
+		UGameplayStatics::SpawnSoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+
+	CasingMesh->SetNotifyRigidBodyCollision(false);
 	
 }
--- ShooterGame/Weapon/BulletCasing.h
+++ ShooterGame/Weapon/BulletCasing.h
@@ -15,14 +15,26 @@ public:
 	// Sets default values for this actor's properties
 	ABulletCasing();
 
+
 protected:
 
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 private: 
 
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* CasingMesh;
+
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpulse;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ShellSound;
+	
+
 
 
 };
